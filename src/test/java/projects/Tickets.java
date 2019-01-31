4131f6f46f1d290f7a546c326b759c9fd6eb6ad7
sannila4369@gmail.com <sannila@1>
2019-01-31 18:55:45 +0530

    Revert "cart-pop_up-functionality-added"
    This reverts commit 0a3bfa18e9b2f0dd544a3b89f151323fc7beae8c.

--- src/test/java/projects/Tickets.java
+++ src/test/java/projects/Tickets.java
@@ -1,20 +1,11 @@
 package projects;
 
 import basePack.BaseClass;
-import basePack.Cart_functionality;
-import basePack.Tickets_Functionality;
 
 public class Tickets extends BaseClass {
 
-    public void tickets() throws Exception {
+    public void tickets(){
 
-        driver.navigate().to(base_url + element.getString("ticket"));
-
-        Tickets_Functionality tickets = new Tickets_Functionality();
-        tickets.tickets();
-
-        Cart_functionality cart = new Cart_functionality();
-        cart.go_to_cart();
     }
 
 }
